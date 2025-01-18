3acbb4b1aa5be773ec0ed720db8df2b84fa85074
Azerino Yogananta Gatot Subroto <>
2025-01-18 13:39:39 +0700

    feat: change to vercel requirement

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
-// import { handle } from "hono/vercel";
+import { handle } from "hono/vercel";
 
 import { AppDataSource } from "./database/data-source";
 import categoryRoute from "./routes/category-route";
@@ -23,13 +23,13 @@ app
   .route("/api/categories", categoryRoute)
   .onError(errorMiddleware);
 
-export default app;
+// export default app;
 
 // for vercel use
-// const handler = handle(app);
+const handler = handle(app);
 
-// export const GET = handler;
-// export const POST = handler;
-// export const PATCH = handler;
-// export const PUT = handler;
-// export const OPTIONS = handler;
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
