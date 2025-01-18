5402e34170febffdb2e4fb808df03efd015c7705
Azerino Yogananta Gatot Subroto <>
2025-01-18 14:54:41 +0700

    feat: remove vercel requirement

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,6 @@
 import { Hono } from "hono";
 import { cors } from "hono/cors";
 import { logger } from "hono/logger";
-import { handle } from "hono/vercel";
 
 import { AppDataSource } from "./database/data-source";
 import categoryRoute from "./routes/category-route";
@@ -23,14 +22,4 @@ app
   .route("/categories", categoryRoute)
   .onError(errorMiddleware);
 
-// export default app;
-
-// for vercel use
-const handler = handle(app);
-
-export const runtime = 'edge'
-export const GET = handler;
-export const POST = handler;
-export const PATCH = handler;
-export const PUT = handler;
-export const OPTIONS = handler;
+export default app;
